4fc2dc17436635418ccbffd853fccb2d87b4cd77
Sarah Roberts <sonoran.sarah@gmail.com>
2024-08-02 13:32:00 -0700

    changed the version to 0.5.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/dire "0.5.5"
+(defproject org.cyverse/dire "0.5.6-SNAPSHOT"
   :description "Erlang-style supervisor error handling for Clojure"
   :url "https://github.com/cyverse-de/dire"
   :license {:name "Eclipse Public License"
